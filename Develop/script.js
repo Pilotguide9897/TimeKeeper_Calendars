a48ac0d2bbd9d46504d8a5eccf03372f9f74365b
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-02-13 11:30:00 -0400

    added reset button/dayjs auto update

--- Develop/script.js
+++ Develop/script.js
@@ -14,23 +14,13 @@ $(document).ready(function () {
   });
 });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  // // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+// To allow users to easily clear the data saved in local storage
+document.getElementById('resetButton').addEventListener('click', clearStorage);
+function clearStorage(){
+  localStorage.clear();
+};
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  
   var currentHour = dayjs().hour();
   console.log(currentHour);
 
@@ -51,6 +41,7 @@ $(document).ready(function () {
 
   document.getElementById("currentDay").innerHTML = dayjs().format("dddd, MMMM, D, h:mm A");
 
-
-
-
+  window.setInterval(function() {
+    document.getElementById("currentDay").innerHTML = dayjs().format("dddd, MMMM, D, h:mm A");
+  }, 60000);
+  
